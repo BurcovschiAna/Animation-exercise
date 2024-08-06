b5802fa502195553234de24b84c634991a88c694
Ana Burcovschi <burcovschi.ana2001@gmail.com>
2024-08-06 18:10:56 +0300

    Update script.js

--- Slider CSS + JS/script.js
+++ Slider CSS + JS/script.js
@@ -1,8 +1,6 @@
+// Declaring all variables
 const prevBtnAboutNews = document.getElementById("prev-btn-about-news");
 const netxBtnAboutNews = document.getElementById("next-btn-about-news");
-prevBtnAboutNews.addEventListener("click", prevSlideAboutNews);
-netxBtnAboutNews.addEventListener("click", nextSlideAboutNews);
-
 let aboutNewsItems = [];
 for (let i = 1; i <= 3; i++) {
   aboutNewsItems.push(document.getElementById(`item-${i}`));
@@ -11,6 +9,11 @@ for (let i = 1; i <= 3; i++) {
 let delays = [0, -6, -12];
 let animation_delay = 6;
 
+// Add event listeners to buttons
+prevBtnAboutNews.addEventListener("click", prevSlideAboutNews);
+netxBtnAboutNews.addEventListener("click", nextSlideAboutNews);
+
+// Update the animation
 function updateAnimationDelays() {
     for (let i = 0; i < aboutNewsItems.length; i++) {
       aboutNewsItems[i].style.animationDelay = `${delays[i]}s`;
@@ -18,12 +21,13 @@ function updateAnimationDelays() {
 }
 updateAnimationDelays();
 
+// Navigate to the previous slide
 function prevSlideAboutNews() {
     delays = delays.map(delay => delay + animation_delay); 
     updateAnimationDelays();
 }
-  
 
+// Navigate to the next slide
 function nextSlideAboutNews() {
     delays = delays.map(delay => delay - animation_delay); 
     updateAnimationDelays();
